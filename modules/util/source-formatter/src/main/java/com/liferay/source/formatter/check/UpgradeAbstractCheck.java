c95a357298c48d97c298a3e185c7c80f3aae00be
Alan Huang <alan.huang@liferay.com>
2022-02-10 20:14:29 +0800

    LPS-144759 Auto SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeAbstractCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeAbstractCheck.java
@@ -49,7 +49,8 @@ public abstract class UpgradeAbstractCheck extends BaseFileCheck {
 
 		LugbotConfig lugbotConfig = YamlFunctions.load(content);
 
-		List<String> upgradeTaskNames = _getConfiguredUpgradeTasks(lugbotConfig);
+		List<String> upgradeTaskNames = _getConfiguredUpgradeTasks(
+			lugbotConfig);
 
 		if (!upgradeTaskNames.contains(checkId)) {
 			SourceFormatterUtil.printError(
@@ -93,9 +94,9 @@ public abstract class UpgradeAbstractCheck extends BaseFileCheck {
 			Path repoPath, LugbotConfig lugbotConfig, Path workspacePath)
 		throws Exception;
 
-	private List<String> _getConfiguredUpgradeTasks(
-		LugbotConfig lugbotConfig) {
+	protected abstract boolean isNeedWorkspace();
 
+	private List<String> _getConfiguredUpgradeTasks(LugbotConfig lugbotConfig) {
 		List<LugbotConfig.Upgrade> upgrades =
 			lugbotConfig.tasks.upgrade.upgrades;
 
@@ -128,8 +129,6 @@ public abstract class UpgradeAbstractCheck extends BaseFileCheck {
 			WorkspaceFunctions::isWorkspacePath
 		);
 	}
-
-	protected abstract boolean isNeedWorkspace();
 
 	private static final String _CHECK_ID = "checkId";
 
