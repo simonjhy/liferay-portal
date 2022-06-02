a585d0703a5bbe2c40b6f8aef7b04ad6cbd91420
Seiphon Wang <seiphon.wang@liferay.com>
2022-06-02 14:46:25 +0800

    LPS-154072 Set default value of docker pull policy as true

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
@@ -1270,7 +1270,7 @@ public class RootProjectConfigurator implements Plugin<Project> {
 
 		String dockerUserName = workspaceExtension.getDockerUserName();
 
-		if (workspaceExtension.getDockerPullPolicy() &&
+		if (!workspaceExtension.getDockerPullPolicy() &&
 			Objects.nonNull(workspaceExtension.getDockerLocalRegistryUrl())) {
 
 			property.set(
@@ -1321,7 +1321,9 @@ public class RootProjectConfigurator implements Plugin<Project> {
 
 		String dockerUserName = workspaceExtension.getDockerUserName();
 
-		if (Objects.nonNull(dockerLocalRegistryUrl)) {
+		if (!workspaceExtension.getDockerPullPolicy() &&
+			Objects.nonNull(dockerLocalRegistryUrl)) {
+
 			property.add(
 				workspaceExtension.getDockerLocalRegistryUrl() + "/" +
 					workspaceExtension.getDockerImageId());
