620a4ad8176cdd172473002730e9e98197c93275
Simon.Jiang <simon.jiang@liferay.com>
2022-06-20 09:08:01 +0800

    LPS-154072 improve

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
@@ -721,6 +721,10 @@ public class RootProjectConfigurator implements Plugin<Project> {
 					List<Dockerfile.Instruction> originalInstructions =
 						new ArrayList<>(instructions.get());
 
+					String dockerLocalRegistryAddress =
+							workspaceExtension.
+								getDockerLocalRegistryAddress();
+					
 					if (Objects.nonNull(workspaceExtension.getProduct())) {
 						WorkspaceExtension.ProductInfo productInfo =
 							workspaceExtension.getProductInfo();
@@ -740,10 +744,6 @@ public class RootProjectConfigurator implements Plugin<Project> {
 									workspaceExtension.getDockerImageLiferay();
 							}
 
-							String dockerLocalRegistryAddress =
-								workspaceExtension.
-									getDockerLocalRegistryAddress();
-
 							if (Objects.nonNull(dockerLocalRegistryAddress)) {
 								dockerImageLiferay = dockerImageLiferay.replace(
 									"liferay", dockerLocalRegistryAddress);
@@ -757,11 +757,17 @@ public class RootProjectConfigurator implements Plugin<Project> {
 						}
 					}
 					else {
+						String dockerImageLiferay = workspaceExtension.
+								getDockerImageLiferay();
+						
+						if (Objects.nonNull(dockerLocalRegistryAddress)) {
+							dockerImageLiferay = dockerImageLiferay.replace(
+								"liferay", dockerLocalRegistryAddress);
+						}
+						
 						Dockerfile.FromInstruction baseImage =
 							new Dockerfile.FromInstruction(
-								new Dockerfile.From(
-									workspaceExtension.
-										getDockerImageLiferay()));
+								new Dockerfile.From(dockerImageLiferay));
 
 						originalInstructions.add(0, baseImage);
 					}
@@ -1056,9 +1062,6 @@ public class RootProjectConfigurator implements Plugin<Project> {
 		else if (Objects.nonNull(dockerUserName)) {
 			repositoryProperty.set(
 				dockerUserName + "/" + workspaceExtension.getDockerImageId());
-		}
-		else {
-			repositoryProperty.set(workspaceExtension.getDockerImageId());
 		}
 
 		Property<String> tagProperty = dockerTagImage.getTag();
