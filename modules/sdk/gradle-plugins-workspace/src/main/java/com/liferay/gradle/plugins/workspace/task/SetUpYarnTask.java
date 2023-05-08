d6c8c646ef7b9a03e720837600e5fc16194e2409
Simon.Jiang <simon.jiang@liferay.com>
2023-05-08 20:03:30 +0800

    LPS-181508 update SetUpYarnTask for gradle 7

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/SetUpYarnTask.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/SetUpYarnTask.java
@@ -62,7 +62,6 @@ public class SetUpYarnTask extends DefaultTask {
 	}
 
 	@OutputFile
-	@PathSensitive(PathSensitivity.RELATIVE)
 	public File getPackageJsonFile() {
 		Project project = getProject();
 
