d8bd1c9f56464c8075714bd216eb61427d0167d8
Simon.Jiang <simon.jiang@liferay.com>
2023-06-07 13:40:15 +0800

    LPS-181508 correctly set relative path bundle file for initBundleTask

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurator/RootProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurator/RootProjectConfigurator.java
@@ -108,6 +108,7 @@ import org.gradle.api.tasks.bundling.Compression;
 import org.gradle.api.tasks.bundling.Tar;
 import org.gradle.api.tasks.bundling.Zip;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
+import org.gradle.util.Path;
 
 /**
  * @author Andrea Di Giorgi
@@ -1127,7 +1128,7 @@ public class RootProjectConfigurator implements Plugin<Project> {
 
 				@Override
 				public void execute(Project project) {
-					_configureDownloadTask(download, workspaceExtension);
+					_configureDownloadTask(project, download, workspaceExtension);
 				}
 
 			});
@@ -1765,7 +1766,7 @@ public class RootProjectConfigurator implements Plugin<Project> {
 				}));
 	}
 
-	private void _configureDownloadTask(
+	private void _configureDownloadTask(Project project, 
 		Download download, WorkspaceExtension workspaceExtension) {
 
 		File destinationDir = workspaceExtension.getBundleCacheDir();
@@ -1784,9 +1785,22 @@ public class RootProjectConfigurator implements Plugin<Project> {
 			if (bundleURLString.startsWith("file:")) {
 				URL url = new URL(bundleURLString);
 
-				File file = new File(url.getFile());
+				Path bundleFilePath = Path.path(url.getPath());
 
-				file = file.getAbsoluteFile();
+				File file = null;
+
+				if (bundleFilePath.isAbsolute()){
+					file = new File(url.getFile());
+
+					file = file.getAbsoluteFile();
+				}
+				else{
+					file = project.file(url.getFile());
+				}
+
+				if (Objects.isNull(file)){
+					return;
+				}
 
 				URI uri = file.toURI();
 
